c51a14c4230f72dd515987b5c975388a31f1c31e
Polillo, M. M <matheus_polillo@yahoo.com>
2019-11-11 18:17:27 -0300

    DateString added

---
+++ PGTData/PGTData/Common/DateString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PGTData.Common
+{
+    public class DateString
+    {
+        public DateTime ToDateTime(string date)
+        {
+            try
+            {
+                string strCultureInfo = "pt-BR";
+                DateTime dateReturn = DateTime.Parse(date, new System.Globalization.CultureInfo(strCultureInfo));
+
+                return dateReturn;
+            }
+            catch (FormatException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
