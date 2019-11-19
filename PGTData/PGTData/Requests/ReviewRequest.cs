47398873d40f0da77a7a3f8f2fc5ab0acae06790
Alexandre Godoy <alexandre.godoy@meiabandeirada.com.br>
2019-11-18 22:25:15 -0300

    fix - review

--- PGTData/PGTData/Requests/ReviewRequest.cs
+++ PGTData/PGTData/Requests/ReviewRequest.cs
@@ -11,9 +11,9 @@ namespace PGTData.Requests
         
         public int ReviewContent { get; set; }
         public int ReviewRelevance { get; set; }
-        public string ReviewResearch { get; set; }
-        public string ReviewMemorial { get; set; }
-        public string ReviewAccording { get; set; }
+        public int ReviewResearch { get; set; }
+        public int ReviewMemorial { get; set; }
+        public int ReviewAccording { get; set; }
 
         public DateTime ReviewDate { get; set; }
         
