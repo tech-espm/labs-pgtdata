b60a569354fa7c7152a0b6eaa93d85069b456b4d
Polillo, M. M <matheus_polillo@yahoo.com>
2019-11-28 22:53:55 -0300

    SubmitRequest parcialmente pronto (não tenho certeza)

---
+++ PGTData/PGTData/Requests/SubmitRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace PGTData.Requests
+{
+    public class SubmitRequest
+    {
+        public int GroupID { get; set; }
+        public string FilePath { get; set; }
+        public string CommentDescription { get; set; }
+        public int ReviewTypeID { get; set; }
+    }
+}
